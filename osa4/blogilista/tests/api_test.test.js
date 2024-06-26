7942d931b40c689a708f15f94bf57e89b60dbf76
Kabskibs <kabs_dev@proton.me>
2024-06-26 07:49:49 +0300

    commit 4.8 - 4.10

---
+++ osa4/blogilista/tests/api_test.test.js
@@ -0,0 +1,69 @@
+const { test, after, describe, beforeEach } = require('node:test')
+const assert = require('node:assert')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const Blog = require('../models/blog')
+const helper = require('./api_test_helper')
+
+const api = supertest(app)
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(helper.initialBlogs)
+})
+
+describe('database tests', () => {
+  test('there are three posts', async () => {
+    const response = await api.get('/api/blogs')
+    assert.strictEqual(response.body.length, helper.initialBlogs.length)
+  })
+})
+
+describe('api tests', () => {
+  test('get - right amount of posts', async () => {
+    const response = await api.get('/api/blogs')
+    assert.strictEqual(response.body.length, helper.initialBlogs.length)
+  })
+
+  test('get - response is in json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('get - first posts title is correct', async () => {
+    const response = await api.get('/api/blogs')
+    const titles = response.body.map(post => post.title)
+    assert.strictEqual(titles[0], helper.initialBlogs[0].title)
+  })
+
+  test('get - bodys id is in right format', async () => {
+    const response = await api.get('/api/blogs')
+    const keys = Object.keys(response.body[0])
+    assert(!keys.includes('_id'))
+  })
+
+  test('post - valid post can be added', async () => {
+    const newBlog = {
+      title: "api_test_valid_post",
+      author: "valid ASD",
+      url: "apitest.test",
+      likes: "30"
+    }
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+    const response = await api.get('/api/blogs')
+    const titles = response.body.map(post => post.title)
+    assert.strictEqual(response.body.length, helper.initialBlogs.length + 1)
+    assert(titles.includes('api_test_valid_post'))
+  })
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})
---
+++ osa4/blogilista/tests/api_test_helper.js
@@ -0,0 +1,26 @@
+const blog = require('../models/blog')
+
+const initialBlogs = [
+  {
+  title: "api_test_5",
+  author: "1 ASD",
+  url: "apitest.test",
+  likes: "1"
+  },
+  {
+    title: "api_test_6",
+    author: "2 ASD",
+    url: "apitest.test",
+    likes: "2"
+  },
+  {
+    title: "api_test_7",
+    author: "3 ASD",
+    url: "apitest.test",
+    likes: "3"
+  }
+]
+
+module.exports = {
+  initialBlogs
+}
