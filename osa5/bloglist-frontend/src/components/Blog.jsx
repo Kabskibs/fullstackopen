4ac634bc0870b55b59b9d418287bd40bf954b525
Kabskibs <kabs_dev@proton.me>
2024-07-12 00:47:50 +0300

    commit 5.7

--- osa5/bloglist-frontend/src/components/Blog.jsx
+++ osa5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,26 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [collapsed, setCollapse] = useState(true)
+  return (
+    <div className="blogPostBox">
+      {collapsed ? (
+        <>
+          {blog.title}
+          <button onClick={ () => setCollapse(false) }>view</button>
+        </>
+      ) : (
+        <>
+          {blog.title}
+          <button onClick={ () => setCollapse(true) }>hide</button><br></br>
+          {blog.url}<br></br>
+          likes {blog.likes}
+          <button>like</button><br></br>
+          {blog.author}
+        </>
+      )}
+    </div>
+  )
+}
 
 export default Blog
