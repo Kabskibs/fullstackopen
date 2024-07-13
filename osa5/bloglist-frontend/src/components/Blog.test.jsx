8df5864a2bcf03ad118b89466cf988cb482006af
Kabskibs <kabs_dev@proton.me>
2024-07-13 23:13:27 +0300

    commit 5.14

--- osa5/bloglist-frontend/src/components/Blog.test.jsx
+++ osa5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 import { expect } from 'vitest'
 
@@ -9,7 +10,7 @@ test('<Blog /> renders title', () => {
   }
   // The ones below are not required for testing,
   // but will cause warnings if not included
-  const user = { username: 'username' }
+  const blogUser = { username: 'username' }
   const funcOne = () => { return null }
   const funcTwo = () => { return null }
   
@@ -17,16 +18,64 @@ test('<Blog /> renders title', () => {
     <Blog
       blog={blog}
       // To avoid warnings, as stated above
-      user={user}
+      user={blogUser}
       handleAddLikes={funcOne}
       handleRemoveBlog={funcTwo}  
     />
   )
 
   const div = container.querySelector('.blogPostBox')
+
   expect(div).toHaveTextContent(
     'Test Title'
   ) && expect(div).toHaveTextContent(
     'Test Author'
+  )
+})
+
+test('<Blog /> view button works and content is rendered', async () => {
+  const blog = {
+    title: 'Test Title',
+    author: 'Test Author',
+    url: 'test.url',
+    user: {
+      name: "Test Name"
+    }
+  }
+  // The ones below are not required for testing,
+  // but will cause warnings if not included
+  const blogUser = { username: 'username' }
+  const funcOne = () => { return null }
+  const funcTwo = () => { return null }
+  
+  const { container } = render(
+    <Blog
+      blog={blog}
+      // To avoid warnings, as stated above
+      user={blogUser}
+      handleAddLikes={funcOne}
+      handleRemoveBlog={funcTwo}  
+    />
+  )
+
+  const div = container.querySelector('.blogPostBox')
+
+  const user = userEvent.setup()
+  const button = screen.getByText('view')
+
+  expect(div).not.toHaveTextContent(
+    'test.url'
+  ) && expect(div).not.toHaveTextContent(
+    'Test Name'
+  )
+  await user.click(button)
+  expect(div).toHaveTextContent(
+    'Test Title'
+  ) && expect(div).toHaveTextContent(
+    'Test Author'
+  ) && expect(div).toHaveTextContent(
+    'test.url'
+  ) && expect(div).toHaveTextContent(
+    'Test Name'
   )
 })
