fd9eea2841a28bc56130f805394efa52c1560440
Kabskibs <kabs_dev@proton.me>
2024-07-13 23:28:38 +0300

    commit 5.15

--- osa5/bloglist-frontend/src/components/Blog.test.jsx
+++ osa5/bloglist-frontend/src/components/Blog.test.jsx
@@ -79,3 +79,40 @@ test('<Blog /> view button works and content is rendered', async () => {
     'Test Name'
   )
 })
+
+test('<Blog /> liking post twice registers as two mock function calls', async () => {
+  const blog = {
+    title: 'Test Title',
+    author: 'Test Author',
+    url: 'test.url',
+    user: {
+      name: "Test Name"
+    }
+  }
+  // The ones below are not required for testing,
+  // but will cause warnings if not included
+  const blogUser = { username: 'username' }
+  const funcOne = () => { return null }
+  
+  const mockHandler = vi.fn()
+  
+  render(
+    <Blog
+      blog={blog}
+      handleAddLikes={mockHandler}
+      // To avoid warnings, as stated above
+      user={blogUser}
+      handleRemoveBlog={funcOne}  
+    />
+  )
+
+  const user = userEvent.setup()
+  const buttonView = screen.getByText('view')
+  await user.click(buttonView)
+
+  const buttonLike = screen.getByText('like')
+  await user.click(buttonLike)
+  await user.click(buttonLike)
+
+  expect(mockHandler.mock.calls).toHaveLength(2)
+})
