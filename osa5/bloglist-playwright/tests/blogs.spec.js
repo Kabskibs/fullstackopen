20e79b9c86067c158ca3005b9f4cc7f1a07feceb
Kabskibs <kabs_dev@proton.me>
2024-07-15 01:30:55 +0300

    commit 5.17

---
+++ osa5/bloglist-playwright/tests/blogs.spec.js
@@ -0,0 +1,25 @@
+const { test, expect, beforeEach, describe } = require('@playwright/test')
+
+describe('Blog app', () => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('http:localhost:3003/api/testing/reset')
+    await request.post('http://localhost:3003/api/users', {
+      data: {
+        name: 'Test User',
+        username: 'test_user1',
+        password: 'test_password'
+      }
+    })
+
+    await page.goto('http://localhost:5173')
+  })
+
+  test('Login form is shown', async ({ page }) => {
+    await expect(page.getByRole('heading', { name: 'login to application' })).toBeVisible();
+    await expect(page.getByText('username')).toBeVisible();
+    await expect(page.locator('input[type="text"]')).toBeVisible();
+    await expect(page.getByText('password')).toBeVisible();
+    await expect(page.locator('input[type="password"]')).toBeVisible();
+    await expect(page.getByRole('button', { name: 'login' })).toBeVisible();
+  })
+})
