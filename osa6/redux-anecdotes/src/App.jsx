c5cc55b36e09a10b9fb15fa494a290d480226661
Kabskibs <kabs_dev@proton.me>
2024-07-19 03:31:07 +0300

    commit 6.16

--- osa6/redux-anecdotes/src/App.jsx
+++ osa6/redux-anecdotes/src/App.jsx
@@ -3,17 +3,14 @@ import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Notification from './components/Notification'
 import Filter from './components/Filter'
-import anecdoteService from './services/anecdotes'
-import { setAnecdotes } from './reducers/anecdoteReducer'
+import { initializeAnecdotes } from './reducers/anecdoteReducer'
 import { useDispatch } from 'react-redux'
 
 const App = () => {
   const dispatch = useDispatch()
+
   useEffect(() => {
-    anecdoteService
-      .getAll()
-      .then(anecdotes => dispatch(setAnecdotes(anecdotes)))
-      // eslint-disable-next-line react-hooks/exhaustive-deps
+    dispatch(initializeAnecdotes())
   }, [])
 
   return (
