1be49e159f6a2edc496be7e095b1c3a2c1a80fab
Kabskibs <kabs_dev@proton.me>
2024-08-04 12:51:32 +0300

    commit 9.1

---
+++ osa9/first_exercises/bmiCalculator.ts
@@ -0,0 +1,13 @@
+const calculateBmi = (height: number, weight: number) => {
+  const heightToM: number = height / 100;
+  const bmi: number = weight / (heightToM * heightToM);
+  if (bmi < 18.5) {
+    console.log("Underweight (unhealthy weight)");
+  } else if (bmi > 18.5 && bmi < 24.9) {
+    console.log("Normal (healthy weight)");
+  } else if (bmi > 24.9) {
+    console.log("Overweight (unhealthy weight)")
+  }
+}
+
+calculateBmi(180, 74);
