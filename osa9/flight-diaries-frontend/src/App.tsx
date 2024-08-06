bc2b381fd61e5f3b0da38c35e9485cee1565e3bb
Kabskibs <kabs_dev@proton.me>
2024-08-06 23:07:47 +0300

    commit 9.18

--- osa9/flight-diaries-frontend/src/App.tsx
+++ osa9/flight-diaries-frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
-import { DiaryEntry, NewDiaryEntry } from './types';
+import { DiaryEntry, NewDiaryEntry, BackendError } from './types';
 
 const App = () => {
   const [entries, setEntries] = useState<DiaryEntry[]>([]);
@@ -9,6 +9,7 @@ const App = () => {
   const [newEntryWeather, setNewEntryWeather] = useState('');
   const [newEntryVisibility, setNewEntryVisibility] = useState('');
   const [newEntryComment, setNewEntryComment] = useState('');
+  const [notification, setNotification] = useState('');
 
   const baseUrl = 'http://localhost:3000/api/diaries';
 
@@ -25,7 +26,14 @@ const App = () => {
     setNewEntryComment('');
   };
 
-  const addNewEntry = (event: React.SyntheticEvent) => {
+  const toggleNotification = (message) => {
+    setNotification(message);
+    setTimeout(() => {
+      setNotification('');
+    }, 5000);
+  };
+
+  const addNewEntry = async (event: React.SyntheticEvent) => {
     event.preventDefault();
     const entryToPost: NewDiaryEntry = {
       date: newEntryDate,
@@ -33,41 +41,60 @@ const App = () => {
       visibility: newEntryVisibility,
       comment: newEntryComment
     };
-    axios
-      .post<DiaryEntry>(baseUrl, entryToPost)
-      .then(response => {
-        setEntries(entries.concat(response.data));
-      });
-    resetFields();
+    try {
+      const response = await axios.post(baseUrl, entryToPost);
+      setEntries(entries.concat(response.data));
+      resetFields();
+    } catch (error) {
+      if (axios.isAxiosError<BackendError, Record<string, unknown>>(error)) {
+        if (error.response) {
+          toggleNotification(error.response.data);
+        } else {
+          toggleNotification(`Error: No error response data: ${error}`)
+        }
+      } else {
+        toggleNotification('Something went wrong.')
+        console.error(error);
+      }
+    }
   };
 
   return (
     <div>
       <div>
         <h2>Add new entry</h2>
-        <form onSubmit={addNewEntry}>
-          date
-          <input
-            value={newEntryDate}
-            onChange={(event) => setNewEntryDate(event.target.value)}
-          /><br></br>
-          visibility
-          <input
-            value={newEntryVisibility}
-            onChange={(event) => setNewEntryVisibility(event.target.value)}
-          /><br></br>
-          weather
-          <input
-            value={newEntryWeather}
-            onChange={(event) => setNewEntryWeather(event.target.value)}
-          /><br></br>
-          comment
-          <input
-            value={newEntryComment}
-            onChange={(event) => setNewEntryComment(event.target.value)}
-          /><br></br>
-          <button type='submit'>add</button>
-        </form>
+        <div style={{ color: 'red' }}>
+          {notification === '' ? (
+            <></>
+          ) : (
+            notification
+          )}
+        </div>
+        <div>
+          <form onSubmit={addNewEntry}>
+            date
+            <input
+              value={newEntryDate}
+              onChange={(event) => setNewEntryDate(event.target.value)}
+            /><br></br>
+            visibility
+            <input
+              value={newEntryVisibility}
+              onChange={(event) => setNewEntryVisibility(event.target.value)}
+            /><br></br>
+            weather
+            <input
+              value={newEntryWeather}
+              onChange={(event) => setNewEntryWeather(event.target.value)}
+            /><br></br>
+            comment
+            <input
+              value={newEntryComment}
+              onChange={(event) => setNewEntryComment(event.target.value)}
+            /><br></br>
+            <button type='submit'>add</button>
+          </form>
+        </div>
       </div>
       <div>
         <h2>Diary Entries</h2>
